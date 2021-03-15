104c98871e26c15c0a1647c2e6e10accb973c1ff
Viachaslau Afanasenka <vafanasenka@allot.com>
2021-03-15 12:34:28 +0300

    revert to DIo

--- lib/src/services/meetup_api_service.dart
+++ lib/src/services/meetup_api_service.dart
@@ -1,13 +1,13 @@
-import 'dart:convert';
 import 'dart:io' show Platform;
 
+import 'package:dio/dio.dart';
 import 'package:flutter_meetuper/src/model/meetup.dart';
-import 'package:http/http.dart';
 
 class MeetupApiService {
+  final Dio dio = Dio();
   final String url = Platform.isIOS
-      ? 'http://localhost:3001/api/v1/meetups'
-      : 'http://10.0.2.2:3001/api/v1/meetups';
+      ? 'http://localhost:3001/api/v1/'
+      : 'http://10.0.2.2:3001/api/v1/';
   static final MeetupApiService _instance = MeetupApiService._internal();
 
   factory MeetupApiService() => _instance;
@@ -15,8 +15,8 @@ class MeetupApiService {
   MeetupApiService._internal();
 
   Future<List<Meetup>> fetchMeetups() async {
-    var response = await get(url);
-    List parsed = jsonDecode(response.body);
+    var response = await dio.get(url + 'meetups');
+    List parsed = response.data;
     return parsed.map((v) => Meetup.fromJSON(v)).toList();
   }
 }
